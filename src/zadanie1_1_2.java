3d8573613f52f0e17a23c75f9af95becf4dd5246
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-04-29 13:23:21 +0300

    Initial commit

---
+++ src/zadanie1_1_2.java
@@ -0,0 +1,9 @@
+// Задание здесь: https://lms.synergy.ru/learning/view/68550/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
+
+public class zadanie1_1_2 {
+    public static void main(String[] args) {
+        System.out.println("Задание: \n1.\t(уберите лишнее) Python подходит для разработки под: backend, frontend, ios \n2.\tbackend, frontend, ios, android  \n3.\tJava подходит для разработки под: ios, android, backend, frontend, ios, a \n4.\tJavaScript подходит для разработки под: backend, frontend, ios, android \n5.\tPhp подходит для разработки под: desktop, web \n6.\tKotlin подходит для разработки под: ios, android, backend, frontend");
+        System.out.println("\nРешение (убрано лишнее): \n1.\t(уберите лишнее) Python подходит для разработки под: backend \n2.\tbackend, frontend, ios, android \n3.\tJava подходит для разработки под: android, backend \n4.\tJavaScript подходит для разработки под: frontend \n5.\tPhp подходит для разработки под: web backend \n6.\tKotlin подходит для разработки под: android, backend");
+
+    }
+}
